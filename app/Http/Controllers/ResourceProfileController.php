37865c8533fd82a0717557affbe252fa29d4f135
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-01-16 11:16:03 +0400

    Fix bugs

---
+++ app/Http/Controllers/ResourceProfileController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ResourceProfile;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ResourceProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return void
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param ResourceProfile $resourceProfile
+     * @return void
+     */
+    public function show(ResourceProfile $resourceProfile)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param ResourceProfile $resourceProfile
+     * @return void
+     */
+    public function edit(ResourceProfile $resourceProfile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param ResourceProfile $resourceProfile
+     * @return void
+     */
+    public function update(Request $request, ResourceProfile $resourceProfile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param ResourceProfile $resourceProfile
+     * @return void
+     */
+    public function destroy(ResourceProfile $resourceProfile)
+    {
+        //
+    }
+}
